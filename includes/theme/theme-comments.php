9e920dbedf92eb500a46983ff127950032082564
PavelPyankov <pyankov@fruitfulcode.com>
2015-08-26 01:01:43 +0300

    fix comments

--- includes/theme/theme-comments.php
+++ includes/theme/theme-comments.php
@@ -7,7 +7,18 @@
 			<li id="comment-<?php echo $comment->comment_ID; ?>" <?php comment_class(); ?>>
 				<?php 
 					$avatar_img = $avatar = '';
-					if (!zoner_validate_gravatar($comment->comment_author_email)) {
+					$has_valid_avatar = FALSE;
+					
+					$hash = md5(strtolower(trim($comment->comment_author_email)));
+					$uri = 'http://www.gravatar.com/avatar/' . $hash . '?d=404';
+					$headers = @get_headers(esc_url($uri));
+					if (!preg_match("|200|", $headers[0])) {
+						$has_valid_avatar = FALSE;
+					} else {
+						$has_valid_avatar = TRUE;
+					}
+					
+					if (!$has_valid_avatar) {
 						 $avatar_img = '<img width="100%" class="img-responsive" data-src="holder.js/86x86?text='. __('Author', 'zoner') .'" alt="" />';
 					} else {
 						 $all_meta_for_user = get_user_meta( $comment->user_id );
@@ -19,7 +30,7 @@
 						if (!empty($avatar))  {
 							$avatar_img = '<img width="100%" class="img-responsive" src="'.$avatar.'" alt="" />';
 						} else {
-							$avatar_img = zoner_commenter_avatar($args);
+							$avatar_img = get_avatar( $comment,  86 );
 						}
 					}
 				?>
@@ -44,28 +55,5 @@
 					<hr>
 				</div><!-- /.comment-content -->
 			<?php
-		}
-	}
-
-	if ( ! function_exists( 'zoner_commenter_avatar' ) ) {
-		function zoner_commenter_avatar( $args ) {
-			global $comment;
-			
-			$avatar = get_avatar( $comment,  86 );
-			return $avatar;
-		}
-	}
-	
-	if ( ! function_exists( 'zoner_validate_gravatar' ) ) {
-		function zoner_validate_gravatar($email) {
-			$hash = md5(strtolower(trim($email)));
-			$uri = 'http://www.gravatar.com/avatar/' . $hash . '?d=404';
-			$headers = @get_headers(esc_url($uri));
-			if (!preg_match("|200|", $headers[0])) {
-				$has_valid_avatar = FALSE;
-			} else {
-				$has_valid_avatar = TRUE;
-			}
-			return $has_valid_avatar;
 		}
 	}
