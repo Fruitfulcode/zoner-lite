88d9c0c200098f402c243cff3ef860d136c35775
Ilya Pavlov <pavlov@fruitfulcode.com>
2016-03-10 14:36:26 +0200

    redux ads removed

--- includes/admin/zoner-options/admin-config.php
+++ includes/admin/zoner-options/admin-config.php
@@ -18,7 +18,11 @@ if (!class_exists('zoner_config')) {
 
         }
 		
+         function disable_redux_notice() {
+		   echo '<style>.redux-notice{ display: none;}</style>';
+		}
         public function initSettings() {
+        	add_action('admin_head', array($this,'disable_redux_notice'));
             $this->theme = wp_get_theme();
             $this->setArguments();
             $this->setHelpTabs();
