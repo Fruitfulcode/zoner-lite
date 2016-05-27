3781850d60fa137a742ed8bfc5fd1a5c5baa7efd
Pavel Pyankov <pyankov@fruitfulcode.com>
2016-05-27 16:57:32 +0300

    Seconadry navigation option fix

--- includes/theme/theme-function.php
+++ includes/theme/theme-function.php
@@ -592,7 +592,7 @@ if ( ! function_exists( 'zoner_seconadry_navigation' ) ) {
 	function zoner_seconadry_navigation() {
 		global $zoner_config;
 		
-		if (empty($zoner_config['show-secondary-nav'])) return;
+		if (isset($zoner_config['show-secondary-nav']) && empty($zoner_config['show-secondary-nav'])) return;
 		
 		$current_user = wp_get_current_user();
 		
